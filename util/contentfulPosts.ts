01664470913ef7004951c1ba29fd61dc96efbc68
Guido van de Riet <guidovdriet@gmail.com>
2020-12-13 20:43:53 +0100

    setup data fetching

--- util/contentfulPosts.ts
+++ util/contentfulPosts.ts
@@ -8,7 +8,13 @@ const client = require("contentful").createClient({
 
 export async function fetchEntries() {
   const entries = await client.getEntries();
+  console.log(entries);
   if (entries.items) return entries.items;
 }
 
-export default { fetchEntries };
+export async function fetchEntry(entry_id: string) {
+  const entry = await client.getEntry(entry_id);
+  if (entry) return entry;
+}
+
+export default { fetchEntries, fetchEntry };
