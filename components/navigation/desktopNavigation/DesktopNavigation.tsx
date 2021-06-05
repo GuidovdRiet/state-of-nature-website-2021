0659803ee13baa39ddfab9a3efb7ef6eec01086c
Guido van de Riet <guido@chainels.com>
2021-06-05 16:08:28 +0200

    added link to newsletter

--- components/navigation/desktopNavigation/DesktopNavigation.tsx
+++ components/navigation/desktopNavigation/DesktopNavigation.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { useRouter } from 'next/router';
 
 // Types
 import { EventType } from '../../../types/EventType';
@@ -38,51 +39,59 @@ export interface NavigationProps {
   navigationData: NavigationType;
 }
 
-const Navigation: FC<NavigationProps> = ({ upcomingEvent, navigationData }) => (
-  <S.Navigation>
-    <img
-      src={navigationData.fields.logo.fields.file.url}
-      alt="State of Nature logo"
-      className="navigation__nav-logo"
-    />
-    <ul className="navigation__nav-items-list">
-      <li>
-        <a>Events</a>
-      </li>
-      <li>
-        <a>Visie</a>
-      </li>
-      <li>
-        <a>Partners</a>
-      </li>
-      <li>
-        <a>Contact</a>
-      </li>
-    </ul>
-    <div className="navigation__right-content-wrapper">
-      <div className="navigation__social-wrapper">
-        <a
-          href="https://www.facebook.com/ourstateofnature"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FacebookIcon className="navigation__social-icon-facebook" />
-        </a>
-        <a
-          href="https://www.instagram.com/ourstateofnature"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <InstagramIcon className="navigation__social-icon-instagram" />
-        </a>
+const Navigation: FC<NavigationProps> = ({ upcomingEvent, navigationData }) => {
+  const router = useRouter();
+
+  return (
+    <S.Navigation>
+      <img
+        src={navigationData.fields.logo.fields.file.url}
+        alt="State of Nature logo"
+        className="navigation__nav-logo"
+      />
+      <ul className="navigation__nav-items-list">
+        <li>
+          <a>Events</a>
+        </li>
+        <li>
+          <a>Visie</a>
+        </li>
+        <li>
+          <a>Partners</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+      </ul>
+      <div className="navigation__right-content-wrapper">
+        <div className="navigation__social-wrapper">
+          <a
+            href="https://www.facebook.com/ourstateofnature"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FacebookIcon className="navigation__social-icon-facebook" />
+          </a>
+          <a
+            href="https://www.instagram.com/ourstateofnature"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <InstagramIcon className="navigation__social-icon-instagram" />
+          </a>
+        </div>
+        {upcomingEvent ? (
+          <Button type="button" text="Tickets" />
+        ) : (
+          <Button
+            type="button"
+            text="Newsletter"
+            onClick={() => window.open('https://eepurl.com/gLtD-T', '_blank')}
+          />
+        )}
       </div>
-      {upcomingEvent ? (
-        <Button type="button" text="Tickets" />
-      ) : (
-        <Button type="button" text="Newsletter" />
-      )}
-    </div>
-  </S.Navigation>
-);
+    </S.Navigation>
+  );
+};
 
 export default Navigation;
