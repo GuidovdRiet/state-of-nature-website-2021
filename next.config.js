f203e1844e988531104360af85a809e5c7abf513
Guido van de Riet <guido@chainels.com>
2022-08-14 18:15:25 +0200

    added temp menu

--- next.config.js
+++ next.config.js
@@ -8,6 +8,22 @@ module.exports = {
     if (!isServer) {
       config.resolve.fallback.fs = false;
     }
+
+    // load worker files as a urls with `file-loader`
+    config.module.rules.unshift({
+      test: /pdf\.worker\.(min\.)?js/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[contenthash].[ext]',
+            publicPath: '_next/static/worker',
+            outputPath: 'static/worker',
+          },
+        },
+      ],
+    });
+
     return config;
   },
 };
--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,26 +1,41 @@
-import { css, Global, useTheme } from '@emotion/react';
+import { css, Global } from '@emotion/react';
+import { useState } from 'react';
+// import default react-pdf entry
+import { Document, Page, pdfjs } from 'react-pdf';
+// import pdf worker as a url, see `next.config.js` and `pdf-worker.js`
+import workerSrc from '../pdf-worker';
 
-function Menu() {
+pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
+
+export default function PDFViewer() {
+  const [file, setFile] = useState('./files/menukaart-hx-hoogcruts-DEF.pdf');
+  const [numPages, setNumPages] = useState(null);
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
+
   return (
-    <>
-      <Global
-        styles={css`
-          body {
-            margin: 0;
-          }
-
-          iframe {
-            width: 100%;
-            height: 100vh;
-          }
-        `}
-      />
-      <iframe
-        src="/files/menukaart-hx-hoogcruts-DEF.pdf"
-        title="Menu kaart HX Hoogcruts"
-      />
-    </>
+    <div>
+      <div>
+        <Global
+          styles={css`
+            body {
+              margin: 0;
+            }
+          `}
+        />
+        <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+          {Array.from({ length: numPages }, (_, index) => (
+            <Page
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              renderAnnotationLayer={false}
+              renderTextLayer={false}
+            />
+          ))}
+        </Document>
+      </div>
+    </div>
   );
 }
-
-export default Menu;
